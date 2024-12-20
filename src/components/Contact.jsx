27ea2a9b1f72ff0c6c1c5820f11606d0bdbf3f28
TuoNomeUtente <mighty_sham>
2024-12-20 13:27:33 +0100

    changed anchor tags to links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,15 +3,15 @@ function Contact() {
         <div className="max-w-lg mx-auto ">
             <p className="text-3xl text-center text-white">Contact me</p>
             <div>
-                <label htmlFor="first_name" className="block mb-2 text-sm font-medium font-light text-gray-500 dark:text-white">First name</label>
+                <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-500 dark:text-white">First name</label>
                 <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mighty" required></input>
             </div>
             <div>
-                <label htmlFor="last_name" className="block mb-2 text-sm font-medium font-light text-gray-500 dark:text-white">Last name</label>
+                <label htmlFor="last_name" className="block mb-2 text-sm font-medium  text-gray-500 dark:text-white">Last name</label>
                 <input type="text" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Shambel" required></input>
             </div>
             <div>
-                <label htmlFor="company" className="block mb-2 text-sm font-medium font-light text-gray-500 dark:text-white">Message</label>
+                <label htmlFor="company" className="block mb-2 text-sm font-medium  text-gray-500 dark:text-white">Message</label>
                 <textarea type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="message" required></textarea>
             </div>  
             <div className="flex justify-center">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,20 @@
+ import { Link } from "react-router-dom";
 function Footer() {
     return <div className="fixed bottom-0 left-0 z-20 w-full p-4  border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600 bg-cyan-950" >
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="#" className=" text-white hover:hover:text-cyan-400 me-4 md:me-6">About</a>
+            <Link href="#" className=" text-white hover:hover:text-cyan-400 me-4 md:me-6">About</Link>
         </li>
         <li>
-            <a href="#" className=" text-white hover:hover:text-cyan-400 me-4 md:me-6">Projects</a>
+            <Link href="#" className=" text-white hover:hover:text-cyan-400 me-4 md:me-6">Projects</Link>
         </li>
         <li>
-            <a href="#" className=" text-white hover:hover:text-cyan-400 me-4 md:me-6">Certificates</a>
+            <Link href="#" className=" text-white hover:hover:text-cyan-400 me-4 md:me-6">Certificates</Link>
         </li>
         <li>
-            <a href="#" className=" text-white hover:hover:text-cyan-400">Contact</a>
+            <Link href="#" className=" text-white hover:hover:text-cyan-400">Contact</Link>
         </li>
     </ul>
     </div>
